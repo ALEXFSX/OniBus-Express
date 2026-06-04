16d07674d13a0b97fb2618cf2f1175580dc168e3
alexfrs.dev@gmail.com <alexfrs.dev@gmail.com>
2026-06-04 16:10:20 -0300

    feat: adiciona o componente Hero

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './index.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-page">
       <Header />
-      <main className="flex-1" />
+      <main className="flex-1">
+        <Hero />
+      </main>
       <Footer />
     </div>
   )
